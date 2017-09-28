4d3b0abfc7bccd12eac3b2fe8be6fa10adcbba2d
raikaat <raikaat@gmail.com>
2017-09-28 20:38:36 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.arr = [];
+        this.length = null;
+        this.middle = null;
+    
+    }
 
     setRange(min, max) {
-
+    this.length = max-min;
+    for (var i = 0; i <=this.length; i++) {
+    this.arr[i]=min+i;
+    }
+	this.middle = Math.floor(this.arr.length/2);
     }
 
     guess() {
-
+    return this.arr[this.middle];
     }
 
     lower() {
-
+    return this.middle = Math.floor((this.middle+1+this.max)/2);  
     }
 
     greater() {
-
+    return this.middle = Math.floor((this.min+this.middle-1)/2);
     }
 }
 
